bc8dce37ec808d9326a73084bc3f09587c9f52ed
Yuji Saikai <saikai@me.com>
2022-08-31 09:41:30 +1000

    Update fbo.py

--- fbo.py
+++ fbo.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
   X0 = torch.tensor(X0s[seed]).unsqueeze(0) # seed selects a row of X0
   X = torch.clone(X0)
   Y = f(X).unsqueeze(-1)
-  train_Y = Y - Y.mean()
+  train_Y = Y - Y.mean() # = 0
 
   """BO steps"""
   pyro.set_rng_seed(seed) # set a seed in torch, random, and numpy
@@ -223,6 +223,6 @@ if __name__ == "__main__":
     Y_next = f(X_next).unsqueeze(-1)
     X = torch.cat((X, X_next))
     Y = torch.cat((Y, Y_next))
-    train_Y = -1 * (Y-Y.mean())/Y.std() # Standardisation
+    train_Y = -1 * (Y-Y.mean())/Y.std() # Standardisation. -1 is for maximisation.
 
   print(Y.detach().numpy().squeeze())
